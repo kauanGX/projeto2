80ab4b68de6fd246ea57a7b55e07a352e6aeeb00
kauan josé <kjens@discente.ifpe.edu.br>
2024-10-19 06:49:26 -0300

    texte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+ import '../global.css'
+ import '@fontsource-variable/roboto-condensed';
+ import '@fontsource-variable/roboto-condensed';
+ import '@fontsource-variable/cabin';
+</script>
+
+<slot/>
